1e0d274f67092917e40f7a72626f25ad2b170781
zhanghanwei <33750720@qq.com>
2018-08-02 14:46:49 +0000

    User can upload avatar

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -12,9 +12,19 @@
         @include('common.error')
         <div class="panel-body">
 
-            <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8">
+            <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">
                 <input type="hidden" name="_method" value="PUT">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group">
+                    <label for="" class="avatar-label">用户头像</label>
+                    <input type="file" name="avatar">
+
+                    @if($user->avatar)
+                        <br>
+                        <img class="thumbnail img-responsive" src="{{ $user->avatar }}" width="200" />
+                    @endif
+                </div>
 
                 <div class="form-group">
                     <label for="name-field">用户名</label>
