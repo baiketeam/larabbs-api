c624a6e3146fdb57f4d3f8e5fd8789ad00d97fbe
zhanghanwei <33750720@qq.com>
2018-08-02 13:24:24 +0000

    Basic user profile editing

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -16,10 +16,10 @@
                     <div class="media-body">
                         <hr>
                         <h4><strong>个人简介</strong></h4>
-                        <p>一个狂热的好战分子. </p>
+                        <p>{{ $user->introduction }}</p>
                         <hr>
                         <h4><strong>注册于</strong></h4>
-                        <p>January 01 1901</p>
+                        <p>{{ $user->created_at->diffForHumans() }}</p>
                     </div>
                 </div>
             </div>
