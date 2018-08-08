16cafb61d001b5a9ade44826fb0d1982e8764766
zhanghanwei <33750720@qq.com>
2018-08-08 14:37:08 +0000

    ceshi2

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -21,10 +21,11 @@ class TopicsController extends Controller
 
 	public function index(Request $request,Topic $topic, User $user, Link $link)
 	{
-		$topics = $topic->withOrder($request->order)->paginate(20);
-        $active_users = $user->getActiveUsers();
-        $links = $link->getAllCached();
-		return view('topics.index', compact('topics', 'active_users', 'links'));
+		// $topics = $topic->withOrder($request->order)->paginate(20);
+  //       $active_users = $user->getActiveUsers();
+  //       $links = $link->getAllCached();
+		// return view('topics.index', compact('topics', 'active_users', 'links'));
+        return "主页";
 	}
 
     public function show(Topic $topic, Request $request)
