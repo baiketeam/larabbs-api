a40ef9d5adc32cdda231df38df0f5164e68ca21d
zhanghanwei <33750720@qq.com>
2018-08-19 13:17:20 +0000

    api/user

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api) {
 
     $api->group([
@@ -28,26 +29,34 @@ $api->version('v1', [
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
     ], function($api) {
+        // 游客可以访问的接口
+
+        // 需要token验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登陆用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+            });
+        });
         // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
-    });
-    // 图片验证码
-    $api->post('captchas', 'CaptchasController@store')
-        ->name('api.captchas.store');
-    // 第三方登陆
-    $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
-        ->name('api.socials.authorizations.store');
-    // 登陆
-    $api->post('authorizations', 'AuthorizationsController@store')
-        ->name('api.authorizations.store');
-    // 刷新token
-    $api->put('authorizations/current', 'AuthorizationsController@update')
-        ->name('api.authorizations.update');
-    // 删除token
-    $api->delete('authorizations/current', 'AuthorizationsController@destroy')
-        ->name('api.authorizations.destroy');
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+        // 图片验证码
+        $api->post('captchas', 'CaptchasController@store')
+            ->name('api.captchas.store');
+        // 第三方登陆
+        $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+            ->name('api.socials.authorizations.store');
+        // 登陆
+        $api->post('authorizations', 'AuthorizationsController@store')
+            ->name('api.authorizations.store');
+        // 刷新token
+        $api->put('authorizations/current', 'AuthorizationsController@update')
+            ->name('api.authorizations.update');
+        // 删除token
+        $api->delete('authorizations/current', 'AuthorizationsController@destroy')
+            ->name('api.authorizations.destroy');
 });
