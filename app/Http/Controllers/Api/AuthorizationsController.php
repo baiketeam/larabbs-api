6b6fbc62b56db07fc924a93df61b4373a34c8dbf
zhanghanwei <33750720@qq.com>
2018-08-25 07:28:20 +0000

    social login change to Passport

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -12,9 +12,12 @@ use Zend\Diactoros\Response as Psr7Response;
 use Psr\Http\Message\ServerRequestInterface;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use League\OAuth2\Server\AuthorizationServer;
+use App\Traits\PassportToken;
 
 class AuthorizationsController extends Controller
 {
+    use PassportToken;
+
     public function socialStore($type, SocialAuthorizationRequest $request)
     {
         if (!in_array($type, ['weixin'])) {
@@ -63,9 +66,11 @@ class AuthorizationsController extends Controller
                 break;
         }
 
-        $token = Auth::guard('api')->fromUser($user);
+        // $token = Auth::guard('api')->fromUser($user);
         // return $this->response->array(['token' => $user->id]);
-        return $this->respondWithToken($token)->setStatusCode(201);
+        // return $this->respondWithToken($token)->setStatusCode(201);
+        $result = $this->getBearerTokenByUser($user, '1', false);
+        return $this->response->array($result)->setStatusCode(201);
     }
 
     // public function store(AuthorizationRequest $request)
